23e1ffbac7db66f56efcbbca027c535953520b55
Lu Duke <dukelu999@gmail.com>
2026-08-31 20:30:39 +0700

    week11update

--- week10/my-react-frontend-01/src/App.jsx
+++ week10/my-react-frontend-01/src/App.jsx
@@ -1,25 +1,14 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Item from "./Item";
 
-function App() {
-    const [message, setMessage] = useState("...Loading...");
-
-    async function fetchData() {
-        const result = await fetch("http://localhost:3000/api/hello");
-        const data = await result.json();
-
-        setMessage(data.message);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    return (
-        <div>
-            Message: {message}
-        </div>
-    );
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="item" element={<Item />} />
+      </Routes>
+    </HashRouter>
+  );
 }
-
-export default App;
